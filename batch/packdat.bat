52b79a3d44902973304984a6000277a52fb011d2
NovaRain <novarain@gmail.com>
2024-04-23 22:30:00 +0800

    Fixed packdat.bat for dat2.exe not in PATH but in the same folder

--- batch/packdat.bat
+++ batch/packdat.bat
@@ -18,11 +18,15 @@ set "datpath=%~dp1"
 if "%~2" == "" (
     set "datfile=%~nx1.dat"
 ) else (
-    set "datfile=%~nx2"
+    set "datfile=%~nx2.dat"
 )
 
 del "%~dp1%datfile%.bak" 2>nul
 ren "%~dp1%datfile%" "%datfile%.bak" 2>nul
 cd /d "%~dpnx1"
-dat2.exe a -r "..\%datfile%" *
+if exist "%retdir%\dat2.exe" (
+    "%retdir%\dat2.exe" a -r "..\%datfile%" *
+) else (
+    dat2.exe a -r "..\%datfile%" *
+)
 cd /d "%retdir%"
